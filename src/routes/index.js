3562b9c2603fd5b5c52bacdb51e6ac5fcf351b4c
geraJuarez <geracjuarez@gmail.com>
2019-01-26 20:50:29 -0600

    add route to the Products resource

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const products= require('./products.js');
 const router = express.Router();
 
 router.get('/', (request, response) => {
-  response.json({ info: 'Hola' });
+  response.json({ info: 'Hola a EasyLex' });
 });
+
+router.use('/products', products);
 
 module.exports = router;
