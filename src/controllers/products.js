80eff011e9286c519584ce832a3117f0897a6a7c
geraJuarez <geracjuarez@gmail.com>
2019-01-27 01:43:16 -0600

    Add functionality of calculating prices

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,3 +1,4 @@
+const Receipt = require('../utils/receipt.js');
 const productsService = require('../services/products.js');
 
 const getProducts = async (req, res) => {
@@ -9,6 +10,23 @@ const getProducts = async (req, res) => {
     }
 }
 
+const buyProducts = async (req, res) => {
+    try {
+        var receipt = new Receipt();
+        let itemsId = req.body;
+
+        for (var i = 0; i < itemsId.length; i++) {
+            let itemInfo = await productsService.getProductsById(itemsId[i]);
+            receipt.addItem(itemsId[i], itemInfo);
+        }
+        
+        res.status(200).json(receipt);
+    } catch (err) {
+        res.status(404).json(err);
+    }
+}
+
 module.exports = {
     getProducts,
+    buyProducts,
 };
--- src/services/products.js
+++ src/services/products.js
@@ -2,8 +2,13 @@ const db = require('./database.js')
 
 const getProducts = async () => {
     return db.Products;
-  };
+};
+
+const getProductsById = async (productId) => {
+    return db.Products[productId];
+};
 
 module.exports = {
     getProducts,
+    getProductsById,
 };
