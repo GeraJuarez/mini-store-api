3aa23c5c4d21c5f96f558e464501e2933d521622
geraJuarez <geracjuarez@gmail.com>
2019-01-26 22:18:38 -0600

    add buyProduct endpoint

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,5 +3,6 @@ const productsController = require('../controllers/products.js');
 const router = express.Router();
 
 router.get('/', productsController.getProducts);
+router.post('/buy', productsController.buyProducts);
 
 module.exports = router;
