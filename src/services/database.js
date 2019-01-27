f715e58785a600534030ac0b6cfb4a2b39a17f6f
geraJuarez <geracjuarez@gmail.com>
2019-01-26 20:50:00 -0600

    init db

---
+++ src/services/database.js
@@ -0,0 +1,18 @@
+const Products = {
+    PANTS: {
+        name: 'Pants',
+        price: 5.0,
+    },
+    TSHIRT: {
+        name: 'T-Shirt',
+        price: 20.0,
+    },
+    HAT: {
+        name: 'Hat',
+        price: 7.5,
+    }
+}
+
+module.exports = {
+    Products,
+}
