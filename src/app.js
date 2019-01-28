75d85dc423206fd276556d9ddf19f7dab7a48445
geraJuarez <geracjuarez@gmail.com>
2019-01-28 13:33:18 -0600

    remove version route

--- src/app.js
+++ src/app.js
@@ -4,13 +4,12 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const port = 8080;
-const version = 'v1';
 const routes = require('./routes/index.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(`/${version}/`, routes);
+app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`); // eslint-disable-line
